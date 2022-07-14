ecbd6221df16a7d03963fcba21b30de47eaf9c17
Ali DOUKKALI <ali.doukkali@exakis-nelite.com>
2022-07-14 17:58:17 +0100

    Dash

---
+++ dashboard.py
@@ -0,0 +1,78 @@
+import cv2
+import sys
+import time
+import torch
+import numpy as np
+import pandas as pd
+from collections import deque
+
+from flask import Flask, Response
+
+from dash import Dash, html, dcc
+from flask import Flask
+
+import dash_bootstrap_components as dbc
+
+server = Flask(__name__)
+
+app = Dash(__name__,server = server, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+
+# _____________________ Video feeds___________________
+
+def time_synchronized():
+    torch.cuda.synchronize() if torch.cuda.is_available() else None
+    return time.time()
+
+class VideoCamera(object):
+    def __init__(self):
+        global res;
+        self.video = cv2.VideoCapture(sys.argv[1])
+        res = f"{int(self.video.get(cv2.CAP_PROP_FRAME_WIDTH))} x {int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))}" 
+
+    def __del__(self):
+        self.video.release()
+        cv2.destroyAllWindows()
+
+    def get_frame(self):
+        
+        success, image = self.video.read()
+        
+
+           
+        if success:
+            ret,buffer=cv2.imencode('.jpg',image)
+            image=buffer.tobytes()
+        return image
+        
+
+def gen(camera):
+    fps = 0.0
+    while True:
+        frame = camera.get_frame()
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@server.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+
+
+
+app.layout = html.Div([
+    html.Img(src="/video_feed")
+
+])
+
+
+
+
+
+
+
+if __name__ =='__main__':
+    app.run_server(port = 8050)
